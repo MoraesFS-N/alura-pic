ca81e856e7f982502953cd7e87e3949d5c46fedc
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-01 11:09:42 -0300

    criando componente de search e inicio da implementação do efeito de hover

--- alura-pic/src/app/photos/photo-list/photo-list.component.ts
+++ alura-pic/src/app/photos/photo-list/photo-list.component.ts
@@ -1,21 +1,18 @@
 import { PhotoService } from './../photo/photo.service';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo';
 
 @Component({
-selector: 'app-photo-list',
+  selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = false;
   currentPage: number = 1;
   userName: string = '';
@@ -29,16 +26,6 @@ export class PhotoListComponent implements OnInit, OnDestroy {
 
     this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
-
-    this.debounce
-    .pipe(debounceTime(300))
-    .subscribe(filter => this.filter = filter);
-  }
-
-
-  ngOnDestroy(): void {
-
-    this.debounce.unsubscribe();
   }
 
   load(){
@@ -46,6 +33,7 @@ export class PhotoListComponent implements OnInit, OnDestroy {
     this.photoService
       .listFromUserPaginated(this.userName, ++this.currentPage)
       .subscribe(photos => {
+            this.filter = ''
             this.photos = this.photos.concat(photos);
             if (!photos.length) this.hasMore = false;
 
