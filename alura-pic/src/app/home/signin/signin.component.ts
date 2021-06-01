ec084cfbcd58336e275f0c6853e9e8d8484a4c39
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-01 16:45:00 -0300

    implementação da nova tela de login

--- alura-pic/src/app/home/signin/signin.component.ts
+++ alura-pic/src/app/home/signin/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './signin.component.html',
@@ -6,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+
+  loginForm: FormGroup;
+
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
+    this.loginForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
   }
 
 }
