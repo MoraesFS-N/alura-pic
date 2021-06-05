8230fa75b550a1a83e96f2b231a11ccdb111b06f
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-05 10:37:45 -0300

    ajustando componente de inclusão de foto

--- alura-pic/src/app/photos/photo-form/photo-form.module.ts
+++ alura-pic/src/app/photos/photo-form/photo-form.module.ts
@@ -1,13 +1,21 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { PhotoFormComponent } from "./photo-form.component";
+import { VMessageModule } from 'src/app/shared/components/vmessage/vmessagem.module';
 
 @NgModule({
   declarations: [
     PhotoFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    VMessageModule,
+    FormsModule
+  ],
+  exports: [
+    PhotoFormComponent
   ]
 })
 export class PhotoFormModule {
