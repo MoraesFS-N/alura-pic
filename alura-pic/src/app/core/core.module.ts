523635ba760f4abf03ace8ebabe99d7f4d605249
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-08 15:55:09 -0300

    adicionando progress bar nas transições entre telas

--- alura-pic/src/app/core/core.module.ts
+++ alura-pic/src/app/core/core.module.ts
@@ -2,13 +2,14 @@ import { CommonModule } from "@angular/common";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { LoadingModule } from "../shared/components/loading/loading.module";
 import { RequestInterceptor } from "./auth/request.interceptor";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LoadingModule],
   exports: [HeaderComponent, FooterComponent],
   providers:[{
     provide: HTTP_INTERCEPTORS,
