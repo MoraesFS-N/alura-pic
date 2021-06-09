944d56a53bb702950fb8cdba1864f500f4c9cc0d
moraes.neto <moraesfelicioneto@hotmail.com>
2021-06-09 10:35:57 -0300

    implementando classe de errors

--- alura-pic/src/app/core/core.module.ts
+++ alura-pic/src/app/core/core.module.ts
@@ -1,15 +1,17 @@
+import { ShowIfLoggedModule } from './../shared/directives/show-if-logged/show-if-logged.module';
 import { CommonModule } from "@angular/common";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { LoadingModule } from "../shared/components/loading/loading.module";
+import { MenuModule } from "../shared/components/menu/menu.module";
 import { RequestInterceptor } from "./auth/request.interceptor";
 import { FooterComponent } from "./footer/footer.component";
 import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule, LoadingModule],
+  imports: [CommonModule, RouterModule, LoadingModule, MenuModule, ShowIfLoggedModule],
   exports: [HeaderComponent, FooterComponent],
   providers:[{
     provide: HTTP_INTERCEPTORS,
